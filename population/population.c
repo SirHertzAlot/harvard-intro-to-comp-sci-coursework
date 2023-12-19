0c49439453a4ed3fe1d6b448f3efdd408e8ce343
Joseph Duke <dukjoseph@gmail.com>
2023-12-19 01:47:56 +0000

    Mon, Dec 18, 2023, 8:47 PM -05:00

--- population/population.c
+++ population/population.c
@@ -28,24 +28,23 @@ int main(void)
     while(endSize < 1);
 
     // TODO: Calculate number of years until we reach threshold
-    int calculateYearsTillEndSize(void)
+    int born = startSize / 3;
+    int died = startSize / 4;
+    int lp = 0;
+
+
+    lp = startSize + born + died;
+    printf("Llamas Per Year =: %i", lp);
+
+
+    int years = 1;
+
+    do
     {
-        int born = startSize / 3;
-        int died = startSize / 4;
-        int lp = 0;
-
-        int LlamasPerYear(void){
-            return lp = startSize + born + died;
-            printf("Llamas Per Year =: %i", lp);
-        }
-        int years = 1;
-
-        do
-        {
-            years * lp;
-        }
-        while(startSize < endSize);
+        int result = years * lp;
+        printf("%i", result);
     }
+    while(startSize < endSize);
 
     // TODO: Print number of years
 }
