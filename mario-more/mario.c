bbe6334f7a8457097405b0d4133ecd12e9a213ef
Joseph Duke <dukjoseph@gmail.com>
2023-12-19 04:50:40 +0000

    Mon, Dec 18, 2023, 11:50 PM -05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
+    int height = get_int("Please specify the height of your pyramid!");
+
+    do
+    {
+
+    }while()
     for(int n = 1; n < 8; n++){
         printf("#\n");
         for(int j = n; j < 8; j++)
