183f0c29f18ee860eaa60929bd423eb3ac1ab475
Joseph Duke <dukjoseph@gmail.com>
2023-12-19 05:12:49 +0000

    Tue, Dec 19, 2023, 12:12 AM -05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,11 +6,15 @@ int main(void)
     int n = 8;
 
     for(int i = 0; i < 8; i++){
+        for(int l = i; l <= 0; l--)
+        {
+            printf("#");
+        }
         for(int k = 0; k <= 8 - i; k++)
         {
             printf(" ");
         }
-        for(int j = -i; j < 8; j++)
+        for(int j = -i; j <= 0; j++)
         {
             printf("#");
         }
