e045071845ae39e7177fccd1180a29efe7a4e43a
Joseph Duke <dukjoseph@gmail.com>
2023-12-21 00:26:49 +0000

    Wed, Dec 20, 2023, 7:26 PM -05:00

--- credit/credit/credit.c
+++ credit/credit/credit.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-  long cc;
+  long cc = 0;
   int count = 0;
   int sum = 0;
+  //Grab the first 2 digits and store them for later review.
   long int cardId = cc / pow(10, count - 2);
 
   do
@@ -15,16 +17,9 @@ int main(void)
   while(cc >= 13 && cc <= 16);
 
   //Performing Luhn checksum
-  while(cc >= 100)
+  while(cc > 0)
     {
       int digit = cc % 10;
-
-      //Grab the first 2 digits and store them for later review.
-      if(count < 2)
-      {
-        cardId[count] += digit;
-      }
-
       cc /= 10;
       count++;
 
@@ -43,19 +38,19 @@ int main(void)
   //If sum returns a number divisible by 10, then the card is valid. Afterwards we check the card Id for a match. No match, the card is invalid.
   if(sum % 10 == 0)
   {
-    if((cardId[0] == 3 && cardId[1] == 4) || ((cardId[0] == 3 && cardId[1] == 7) && count == 15))
+    if((cardId == 34 || cardId == 37) && count == 15)
     {
       printf("AMEX\n");
     }
-    else if ((cardId[0] == 5 && cardId[1] == 1) ||
-      (cardId[0] == 5 && cardId[1] == 2) ||
-      (cardId[0] == 5 && cardId[1] == 3) ||
-      (cardId[0] == 5 && cardId[1] == 4) ||
-      ((cardId[0] == 5 && cardId[1] == 5) && count == 16))
+    else if ((cardId == 51 ||
+      cardId == 52 ||
+      cardId == 53 ||
+      cardId == 54 ||
+      cardId == 55) && count == 16)
     {
       printf("MASTERCARD\n");
     }
-    else if ((cardId[0] == 4) && (count == 13 || count == 16))
+    else if (cardId == 4 && (count == 13 || count == 16))
       {
       printf("VISA\n");
       }
