6582d957c8df284ebf7d85a7f8b0a023d64f1c54
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 04:18:04 +0000

    Thu, Dec 21, 2023, 11:18 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
 
 int compute_score(string word);
 
@@ -45,7 +45,7 @@ int compute_score(string word)
             char curChar = word[i];
             char upperChar = toupper(curChar);
 
-            int index = strchr(ALPHABET, word[i]) - ALPHABET;
+            int index = strpos(ALPHABET, word[i]);
 
             printf("index of %c, is %i \n", curChar, index);
         }
