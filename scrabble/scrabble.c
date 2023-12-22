f4f47f2c24bf7fd2cd8c29f8b0c9eca7a7a32021
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 03:06:14 +0000

    Thu, Dec 21, 2023, 10:06 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int ALPHABET[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
 
 int compute_score(string word);
 
@@ -23,5 +24,10 @@ int main(void)
 
 int compute_score(string word)
 {
+    int slen = strlen(word);
+    if(strlen(word) > 0)
+    {
+        for(int i = 0;)
+    }
     // TODO: Compute and return score for string
 }
