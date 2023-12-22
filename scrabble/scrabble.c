ce1254541acdd9df96eb71012912d62f09de11fd
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 03:39:34 +0000

    Thu, Dec 21, 2023, 10:39 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-int ALPHABET[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
+int ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 int compute_score(string word);
 
@@ -41,6 +41,7 @@ int compute_score(string word)
     {
         for(int i = 0; i < slen; i++)
         {
+            string upper = toupper(word[i]);
             string curChar = word[i];
             printf("Letter: %s \n", curChar)
         }
