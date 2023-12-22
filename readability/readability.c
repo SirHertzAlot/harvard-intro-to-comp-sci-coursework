80193b24729fe2b472630a5bc49c5c68c41a3cd2
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 22:14:14 +0000

    Fri, Dec 22, 2023, 5:14 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,30 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 
-int main(void) 
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+
+
+int main(void)
+{
+
+}
+
+
+int count_letters(string text)
+{
+
+}
+
+int count_words(string text)
+{
+
+}
+
+int count_sentences(string text)
 {
 
 }
