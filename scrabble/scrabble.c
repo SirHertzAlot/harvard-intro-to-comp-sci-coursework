16956a72c00c14edd187fb28ca2db1728ad4e542
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 04:30:06 +0000

    Thu, Dec 21, 2023, 11:30 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -41,7 +41,7 @@ int compute_score(string word)
     {
         for(int i = 0; i < slen; i++)
         {
-            const char *curChar = &word[i];
+            const char *curChar = word[i];
 
             size_t index = strcspn(ALPHABET, curChar);
 
