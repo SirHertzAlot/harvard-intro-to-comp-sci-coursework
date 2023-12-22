19ab26051036730637e0b17c11b86773f1e3c077
Joseph Duke <dukjoseph@gmail.com>
2023-12-22 22:28:52 +0000

    Fri, Dec 22, 2023, 5:28 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,8 +28,17 @@ int main(void)
 
 int count_letters(string text)
 {
-    
-    return 0;
+    int count = 0;
+
+    for(int i = 0; i < strlen(text); i++)
+    {
+        char letter = text[i];
+        if(isalpha(letter))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int count_words(string text)
