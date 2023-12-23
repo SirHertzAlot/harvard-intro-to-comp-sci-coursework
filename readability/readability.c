864114bd6bef421815c3fc32d93461372f0709fb
Joseph Duke <dukjoseph@gmail.com>
2023-12-23 01:25:59 +0000

    Fri, Dec 22, 2023, 8:25 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,8 +16,8 @@ int main(void)
     int words =  count_words(text);
     int sentences = count_sentences(text);
 
-    float L = (float)letters / (float)words * 100.0;
-    float S = (float)sentences / (float)words * 100.0;
+    float L = (float) letters / (float) words * 100;
+    float S = (float)sentences / (float)words * 100;
 
     int index = round(0.0588 * L - 0.296 * S - 15.8);
 
