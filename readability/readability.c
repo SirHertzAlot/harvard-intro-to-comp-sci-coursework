29650ea4ee93337fbc11fdd20720b5a16a1534d4
Joseph Duke <dukjoseph@gmail.com>
2023-12-23 01:26:57 +0000

    Fri, Dec 22, 2023, 8:26 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -23,7 +23,7 @@ int main(void)
 
     if(index < 1)
     {
-        printf("Before Grade 1 \n");
+        printf("Before Grade 1\n");
     }else if(index > 16)
     {
         printf("Grade 16+\n");
