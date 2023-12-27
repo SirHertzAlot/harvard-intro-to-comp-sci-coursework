68a84054d1316cd91ff00cb26951f5de5f780f23
Joseph Duke <dukjoseph@gmail.com>
2023-12-27 22:41:17 +0000

    Wed, Dec 27, 2023, 5:41 PM -05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,7 @@ bool vote(string name)
 {
     for(int i = 0; i < candidate_count + 1; i++)
     {
-        if(candidates[i].name == name)
+        if(strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes += 1;
             return true;
