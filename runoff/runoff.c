6b6e63c800e9742f45b8e6cb56e6e779f53186e8
Joseph Duke <dukjoseph@gmail.com>
2023-12-28 00:52:39 +0000

    Wed, Dec 27, 2023, 7:52 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if(strcmp(name, candidates[i].name) == 0)
         {
-            preferences[i][i] = i;
+            preferences[i][i] = rank;
         }
         else if (strcmp(name, candidates[i].name) != 0 )
         {
