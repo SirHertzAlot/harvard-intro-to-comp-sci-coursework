fbf23f8306c4d57c79b32d6080c4bf5fd58fb317
Joseph Duke <dukjoseph@gmail.com>
2023-12-28 02:44:46 +0000

    Wed, Dec 27, 2023, 9:44 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,14 +143,14 @@ bool vote(int voter, int rank, string name)
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
-{
+{   
     for(int i = 0; i < candidate_count; i++)
     {
         if(candidates[i].eliminated != false)
         {
             break;
         }
-        candidates[i].votes++;
+        int votes = candidates[i].votes++;
     }
     return;
 }
