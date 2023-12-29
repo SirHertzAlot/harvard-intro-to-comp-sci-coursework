975640fb939a48680bde95c12a349457d2b33fb4
Joseph Duke <dukjoseph@gmail.com>
2023-12-29 00:12:32 +0000

    Thu, Dec 28, 2023, 7:12 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -62,12 +62,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-     for(int i = height; i > height; i--)
+     for(int i = 0; i < height; i++)
     {
-        for(int j = width; j > width; j--)
+        for(int j = 0; j < width; j++)
         {
             if(image)
             {
+                
                 image[i][j].rgbtRed;
                 image[i][j].rgbtBlue;
                 image[i][j].rgbtGreen;
