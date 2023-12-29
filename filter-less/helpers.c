3bb8b2b5c150e3e90533a1bc92d2ddb67d5b99cf
Joseph Duke <dukjoseph@gmail.com>
2023-12-29 00:00:48 +0000

    Thu, Dec 28, 2023, 7:00 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -28,24 +28,30 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         {
             if(image)
             {
-                int sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-                int sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
-                int sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+                int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+                int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+                int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
 
-                if(sepiaRed > 255)
+                if(round(sepiaRed) > 255)
                 {
                     sepiaRed = 255;
                     image[i][j].rgbtRed = sepiaRed ;
+                } else {
+                    image[i][j].rgbtGreen = sepiaRed;
                 }
 
-                if(sepiaGreen > 255)
+                if(round(sepiaGreen) > 255)
                 {
                     sepiaGreen = 255;
                     image[i][j].rgbtBlue = sepiaGreen;
+                } else {
+                    image[i][j].rgbtGreen = sepiaGreen;
                 }
-                if(sepiaBlue > 255)
+                if(round(sepiaBlue) > 255)
                 {
                     sepiaBlue = 255;
+                    image[i][j].rgbtGreen = sepiaBlue;
+                } else {
                     image[i][j].rgbtGreen = sepiaBlue;
                 }
             }
