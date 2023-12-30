b6ece00e61cfcec901083b3a0097fbca20842562
Joseph Duke <dukjoseph@gmail.com>
2023-12-30 06:29:23 +0000

    Sat, Dec 30, 2023, 1:29 AM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,10 +27,7 @@ int main(int argc, char *argv[])
         if((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && (buffer[3] & 0xf0) == 0xe0)
         {
             while(sprintf(filename, "%03i.jpg", 2) == -1);
-            FILE *img = fopen()
-        } else {
-
-        }
+        } 
     }
     fclose(file);
 }
