c1cb7cfb7989f8ed540570ff615e1e7ebdc0b1e5
Joseph Duke <dukjoseph@gmail.com>
2023-12-30 06:34:41 +0000

    Sat, Dec 30, 2023, 1:34 AM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,14 +22,16 @@ int main(int argc, char *argv[])
 
     BYTE buffer[4];
 
-    int signature[3] = {0xff, 0xd8, 0xff}
+    uint8_t signature[] = {0xff, 0xd8, 0xff}
 
     while (fread(buffer, 1, 4, file) == 4)
     {
-        if((buffer[0] == signature[0]) && (buffer[1] == signature[1]) && (buffer[2] == signature[2]) && (buffer[3] & 0xf0) == 0xe0)
+        if((buffer[0] != signature[0]) && (buffer[1] != signature[1]) && (buffer[2] != signature[2]) && (buffer[3] & 0xf0) == 0xe0)
         {
-            while(sprintf(filename, "%03i.jpg", 2) == -1);
+            printf("Doesn't appear to be a jpeg.");
         }
+
+
     }
     fclose(file);
 }
