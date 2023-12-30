ee290ae8b9a66a2b49d5dab3b2a282e7a1819041
Joseph Duke <dukjoseph@gmail.com>
2023-12-30 05:01:22 +0000

    Sat, Dec 30, 2023, 12:01 AM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -12,10 +12,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         {
             if(image)
             {
-                int avg = round(image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen / 3);
-                image[i][j].rgbtRed = avg;
-                image[i][j].rgbtBlue = avg;
-                image[i][j].rgbtGreen = avg;
+                int avg = image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen / 3;
+                image[i][j].rgbtRed = round(avg);
+                image[i][j].rgbtBlue = round(avg);
+                image[i][j].rgbtGreen = round(avg);
             }
         }
     }
