814037ed113efbad659d147d6c8417b0ec787e08
Joseph Duke <dukjoseph@gmail.com>
2023-12-30 05:35:40 +0000

    Sat, Dec 30, 2023, 12:35 AM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
-
-    typedef uint8_t BYTE;
 
     BYTE buffer[512];
 
     while (fread(buffer, 512, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-
+        printf("Inside while fread.");
     }
-
-
     fclose(file);
 }
