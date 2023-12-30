ead5252b595ba747ae1a0d6fc271783b552aee66
Joseph Duke <dukjoseph@gmail.com>
2023-12-30 02:48:10 +0000

    Fri, Dec 29, 2023, 9:48 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -97,7 +97,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
             if(j == 0)
             {
-                    sumRed += round(img_cpy[i][j].rgbtRed + img_cpy[i + 1][j + 1].rgbtRed) + img_cpy[i + 2][j + 2].rgbtRed) / j;
+                    sumRed += round(img_cpy[i][j].rgbtRed + img_cpy[i + 1][j + 1].rgbtRed + img_cpy[i + 2][j + 2].rgbtRed) / j;
                     sumBlue += round(img_cpy[i][j].rgbtBlue + img_cpy[i + 1][j + 1].rgbtBlue) / j;
                     sumGreen += round(img_cpy[i][j].rgbtGreen + img_cpy[i + 1][j + 1].rgbtGreen) / j;
                     rcount++;
