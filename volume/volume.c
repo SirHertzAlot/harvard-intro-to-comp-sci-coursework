d76716eef4e89358dbe6fd51b32c5d14483ef20d
Joseph Duke <dukjoseph@gmail.com>
2024-01-04 22:09:07 +0000

    Thu, Jan 4, 2024, 5:09 PM -05:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
 
-    while(fread(header, HEADER_SIZE, 1 input))
+    while(fread(header, HEADER_SIZE, 1, input))
     {
         fwrite(&header, HEADER_SIZE, 1, output);
     }
