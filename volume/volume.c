cfe70fe2dc9710544823a420ddd91f32ed49fea7
Joseph Duke <dukjoseph@gmail.com>
2024-01-04 02:23:47 +0000

    Wed, Jan 3, 2024, 9:23 PM -05:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[HEADER_SIZE]
+    uint8_t header[HEADER_SIZE];
+    uint16_t buffer;
 
     // TODO: Read samples from input file and write updated data to output file
 
