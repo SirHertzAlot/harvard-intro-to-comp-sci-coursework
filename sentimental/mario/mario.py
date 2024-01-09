247017d2c836ba67fd1518be4f708118f57c2f55
Joseph Duke <dukjoseph@gmail.com>
2024-01-09 23:38:15 +0000

    Tue, Jan 9, 2024, 6:38 PM -05:00

--- sentimental/mario/mario.py
+++ sentimental/mario/mario.py
@@ -1,9 +1,11 @@
 from cs50 import get_int
 
+hash = "#"
+
 while True:
     height = get_int("Height: ")
     if height > 0:
         break
 
-for i in range(height + 1, 0):
-    print('#' * i)
+for i in range(0, height + 1):
+    print(hash * i)
