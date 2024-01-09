86061ab5f97398c6ef9fc9fca287dbb5eb9b9d3a
Joseph Duke <dukjoseph@gmail.com>
2024-01-09 22:28:34 +0000

    Tue, Jan 9, 2024, 5:28 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -28,7 +28,6 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    const char lower = tolower(*word);
     int value = hash(word);
 
     node *start = table[value];
