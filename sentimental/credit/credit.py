508c2b2e7c5f46584d0574a042a82c577e1ed9f2
Joseph Duke <dukjoseph@gmail.com>
2024-01-10 03:13:10 +0000

    Tue, Jan 9, 2024, 10:13 PM -05:00

--- sentimental/credit/credit.py
+++ sentimental/credit/credit.py
@@ -1,14 +1,32 @@
 from cs50 import get_string
 
+digit = 0
 strcc = 0
 sum = 0
-ccid = 0
+ccid = []
 
 strcc = get_string("card number: ")
 
 cc = list(strcc)
 
-for i in range(0, len(cc))
+for i in range(0, len(cc)):
     ccid.append(cc[0:2])
-    if i % 2 == 0
-        cc[i]
+    if i % 2 == 0:
+        digit *= cc[i]
+        if digit > 9:
+            digit -= 9
+    sum += digit
+
+
+
+if sum % 10 == 0:
+    if ccid == 34 or ccid == 37:
+        print("AMEX\n")
+    elif ccid == 51 or ccid == 52 or ccid == 53 or ccid == 54 or ccid == 55:
+        print("MASTERCARD\n")
+    elif ccid[0] == 4:
+        print("VISA\n")
+    else:
+        print("INVALID\n")
+else:
+    print("INVALID\n")
