b5eb4e93d2852cd65ae8ba44d66dcfe5886032cc
Joseph Duke <dukjoseph@gmail.com>
2024-01-10 03:02:14 +0000

    Tue, Jan 9, 2024, 10:02 PM -05:00

--- sentimental/credit/credit.py
+++ sentimental/credit/credit.py
@@ -2,11 +2,13 @@ from cs50 import get_string
 
 strcc = 0
 sum = 0
+ccid = 0
 
 strcc = get_string("card number: ")
 
 cc = list(strcc)
 
 for i in range(0, len(cc))
-    ifcc[i]
+    ccid.append(cc[0:2])
+    
 
