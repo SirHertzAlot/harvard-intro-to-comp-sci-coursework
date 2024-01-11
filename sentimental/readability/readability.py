f30ff5de4234de4889a1266d8c1503243903f09e
Joseph Duke <dukjoseph@gmail.com>
2024-01-11 00:44:44 +0000

    Wed, Jan 10, 2024, 7:44 PM -05:00

---
+++ sentimental/readability/readability.py
@@ -0,0 +1,28 @@
+import math
+import cs50 
+
+words = 0
+chars = 0
+sent = 0
+
+val = input("Text: ")
+
+sent += val.count(".") + val.count("!") + val.count("?")
+words += len(val.split())
+
+for i in val:
+  chars += len(i.split())
+
+chars -= sent
+
+l = float(chars) / float(words) * 100
+s = float(sent) / float(words) * 100
+
+index = math.floor(0.0588 * l - 0.296 * s - 15.8)
+
+if(index < 1):
+  print("Before Grade 1");
+elif(index > 16):
+  print("Grade 16+");
+else:
+  print("Grade " + str(int(index)))
