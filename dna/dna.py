66510355e6db1337c64c0c0e0c815ed6db8ebcf6
Joseph Duke <dukjoseph@gmail.com>
2024-01-12 02:18:00 +0000

    Thu, Jan 11, 2024, 9:18 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -55,7 +55,7 @@ def str_finder():
     for i in range(len(srts)):
       i = 1
       match = longest_match(dna_seq[0][0], srts[0][i])
-      match += 
+      match += f"{srts[0][i]}"
       profiles.append(match)
 
   # TODO: Check database for matching profiles
