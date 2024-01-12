4a6771cf30d896730961d7afd3ab999781bc5c13
Joseph Duke <dukjoseph@gmail.com>
2024-01-12 04:11:22 +0000

    Thu, Jan 11, 2024, 11:11 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -63,6 +63,9 @@ def profile_matcher():
         srtsToComp = db_val[k].get(profiles[0][0])
         if str(profiles[0][1]) == srtsToComp:
           return print(db_val[k].get("name"))
+    else:
+       return print("No match")
+
 
 
 def longest_match(sequence, subsequence):
