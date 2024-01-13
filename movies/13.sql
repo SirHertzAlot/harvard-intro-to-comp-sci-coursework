befa5b712d632bfb86347e50780281a706581082
Joseph Duke <dukjoseph@gmail.com>
2024-01-13 01:43:08 +0000

    Fri, Jan 12, 2024, 8:43 PM -05:00

---
+++ movies/13.sql
@@ -0,0 +1,1 @@
+SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Kevin Bacon" AND movies.title IN(SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "*";
