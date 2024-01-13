119070a66f0b002cd6c13e875170f2827154cafc
Joseph Duke <dukjoseph@gmail.com>
2024-01-13 01:11:01 +0000

    Fri, Jan 12, 2024, 8:11 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id = (SELECT id FROM movies WHERE year=2004));
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY people.birth;
