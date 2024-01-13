a5bfbf29d11c28e7d810a03d3f5a548f69920abc
Joseph Duke <dukjoseph@gmail.com>
2024-01-13 01:42:00 +0000

    Fri, Jan 12, 2024, 8:42 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,2 @@
 /*In 12.sql, write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.*/
+SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Bradley Cooper" AND movies.title IN(SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Jennifer Lawrence");
