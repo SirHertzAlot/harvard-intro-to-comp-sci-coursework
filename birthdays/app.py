3c919bac400c978c876b5dfb113131a4ebcf164d
Joseph Duke <dukjoseph@gmail.com>
2024-01-22 22:59:41 +0000

    Mon, Jan 22, 2024, 5:59 PM -05:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -24,10 +24,6 @@ def after_request(response):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        
-    else:
-
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
