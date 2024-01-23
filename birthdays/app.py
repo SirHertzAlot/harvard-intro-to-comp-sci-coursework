38cf0072f08541330d2abcc363b182c8a6f933b9
Joseph Duke <dukjoseph@gmail.com>
2024-01-23 00:26:52 +0000

    Mon, Jan 22, 2024, 7:26 PM -05:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -34,7 +34,7 @@ def index():
 
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
-
+        print(birthdays)
         return render_template("index.html")
 
 
