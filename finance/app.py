920a8d1ccae53928e9c91396b97e0db6708b66e1
Joseph Duke <dukjoseph@gmail.com>
2024-01-25 00:41:33 +0000

    Wed, Jan 24, 2024, 7:41 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -13,9 +13,8 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 
-app.config.update(
-    WTF_CSRF_SECRET_KEY = " This is my key."
-)
+app.config['SECRET_KEY'] = 'this is my key'
+
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
