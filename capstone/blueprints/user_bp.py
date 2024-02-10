db455900324f4967b2945f4ce77e5383cbfc4a4e
Joseph Duke <dukjoseph@gmail.com>
2024-02-10 22:29:12 +0000

    Sat, Feb 10, 2024, 5:29 PM -05:00

--- capstone/blueprints/user_bp.py
+++ capstone/blueprints/user_bp.py
@@ -8,5 +8,27 @@ def show(user_id):
     user_posts = db.execute("SELECT * FROM posts WHERE user_id = ?", user_id)
     print(user_posts)
     return """
-    
+    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Timeline</h4>
+                                <div class="timeline-">
+                                    <ul class="timeline">
+                                        <li>
+                                            <div class="timeline-badge primary"></div><a href="#" class="timeline-panel text-muted"><span>10 minutes ago</span><h6 class="m-t-5">Youtube, a video-sharing website, goes live.</h6></a>
+                                        </li>
+                                        <li>
+                                            <div class="timeline-badge warning"></div><a href="#" class="timeline-panel text-muted"><span>20 minutes ago</span><h6 class="m-t-5">Mashable, a news website and blog, goes live.</h6></a>
+                                        </li>
+                                        <li>
+                                            <div class="timeline-badge danger"></div><a href="#" class="timeline-panel text-muted"><span>30 minutes ago</span><h6 class="m-t-5">Google acquires Youtube.</h6></a>
+                                        </li>
+                                        <li>
+                                            <div class="timeline-badge success"></div><a href="#" class="timeline-panel text-muted"><span>15 minutes ago</span><h6 class="m-t-5">StumbleUpon is acquired by eBay.</h6></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 """
