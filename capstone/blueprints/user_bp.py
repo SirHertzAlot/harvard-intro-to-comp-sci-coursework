f41cbc07dfa3f20724a2d7f58e9701e6db0fd66d
Joseph Duke <dukjoseph@gmail.com>
2024-02-10 22:26:46 +0000

    Sat, Feb 10, 2024, 5:26 PM -05:00

--- capstone/blueprints/user_bp.py
+++ capstone/blueprints/user_bp.py
@@ -1,3 +1,12 @@
-from flask import Flask, Blueprint
+from flask import Blueprint
 
-user_bp = Blueprint()
+user_endpoint = Blueprint('user_endpoint', __name__,
+                        template_folder='templates')
+
+@user_endpoint.route('/api/users/<user_id>')
+def show(user_id):
+    user_posts = db.execute("SELECT * FROM posts WHERE user_id = ?", user_id)
+    print(user_posts)
+    return """
+    
+"""
