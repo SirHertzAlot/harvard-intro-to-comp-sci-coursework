8813537d74c0eee66510e6918dc147f5c2227086
Joseph Duke <dukjoseph@gmail.com>
2024-02-11 03:49:09 +0000

    Sat, Feb 10, 2024, 10:49 PM -05:00

--- capstone/templates/ui/create/post_form.py
+++ capstone/templates/ui/create/post_form.py
@@ -1,3 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField
 from wtforms.validators import InputRequired, Length
+
+class PostForm(FlaskForm):
+    post = StringField('Title', validators=[InputRequired(),
+                                             Length(min=10, max=100)])
