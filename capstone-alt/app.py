4b2c8b67a69333be3ba198ee3175891e7037763b
Joseph Duke <dukjoseph@gmail.com>
2024-02-15 21:50:52 +0000

    Thu, Feb 15, 2024, 4:50 PM -05:00

--- capstone-alt/app.py
+++ capstone-alt/app.py
@@ -13,15 +13,3 @@ curr_year = curr_day.year
 @app.route("/")
 def index():
     return render_template("pages/index.html")
-
-@app.route("/api/getcal")
-def getCalView():
-    new_cal = calendar.HTMLCalendar(firstweekday=0)
-    formatted_cal = str(new_cal.formatyear(curr_year))
-    month_view = new_cal.formatmonth(curr_year, curr_month)
-    return render_template("pages/index.html", new_cal=month_view)
-
-@app.route("/api/day")
-def getDayView():
-    hours = genHours()
-    return render_template("pages/index.html", new_day=month_view)
