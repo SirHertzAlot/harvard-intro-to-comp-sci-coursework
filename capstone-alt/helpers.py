1925d57bc1aff094d2cc90681fcd64b2280d7a7a
Joseph Duke <dukjoseph@gmail.com>
2024-05-06 23:58:37 +0000

    Mon, May 6, 2024, 7:58 PM -04:00

--- capstone-alt/helpers.py
+++
@@ -1,8 +0,0 @@
-def genHours():
-    hours = []
-    for hour in range(24):
-        hours.append(hour)
-        if hour > 12:
-            hour - 12
-            hours.append(hour)
-    return hours
